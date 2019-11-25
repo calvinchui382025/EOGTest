31b76e1524513df53ce32ce9ad497714aa7ea711
snyperifle <calvinchui382025@yahoo.com>
2019-11-24 19:19:52 -0600

    testing useQuery

--- src/components/NowWhat.tsx
+++ src/components/NowWhat.tsx
@@ -14,9 +14,6 @@ import Autocomplete from '@material-ui/lab/Autocomplete';
 import { Provider, createClient, useQuery } from 'urql';
 //--------------------------------------------------------------------
 const useStyles = makeStyles({
-  // card: {
-  //   margin: '5% 25%',
-  // },
   input: {
     margin: '30',
   }
@@ -26,7 +23,23 @@ const client = createClient({
   url: 'https://react.eogresources.com/graphql',
 });
 const query = `
-
+query Test{
+	getMultipleMeasurements(input: 
+    [
+      {
+        metricName:"oilTemp"
+      }
+    ]
+  ){
+    metric
+    measurements{
+      metric
+      at
+      value
+      unit
+    }
+  }
+}
 `
 //--------------------------------------------------------------------
 const selections = [
@@ -43,9 +56,15 @@ const selections = [
 const Metrics = () => {
   const classes = useStyles();
 
+  const [result] = useQuery({
+    query,
+  })
+  const { fetching, data, error } = result;
+
   useEffect(() => {
-    console.log('Tester');
-  },[])
+    console.log('Testing');
+    console.log(data);
+  }, [data])
 
   return (
     <div
@@ -78,36 +97,3 @@ export default () => {
     </Provider>
   );
 };
-
-// <Card className={classes.card}>
-// <CardHeader title="OK, Calvin Chui, you're all setup. Now What?" />
-// <CardContent>
-//   <List>
-//     <ListItem>
-//       <Avatar>1</Avatar>
-//       <ListItemText primary="Explore the GraphQL API" />
-//     </ListItem>
-//     <ListItem>
-//       <Avatar>2</Avatar>
-//       <ListItemText primary="Add ability to select Metrics" />
-//     </ListItem>
-//     <ListItem>
-//       <Avatar>3</Avatar>
-//       <ListItemText primary="Display the current metric data" />
-//     </ListItem>
-//     <ListItem>
-//       <Avatar>4</Avatar>
-//       <ListItemText primary="Chart historical metric data" />
-//     </ListItem>
-//     <ListItem>
-//       <Avatar>5</Avatar>
-//       <ListItemText primary="Submit Your App" />
-//     </ListItem>
-//   </List>
-
-//   <Typography variant="body1">
-//     Remember to refer to our <a href="https://react.eogresources.com/assessing">How We Assess Submissions</a>{' '}
-//     guidelines, as well as the <a href="https://react.eogresources.com/api">GraphQL API Documentation</a>.
-//   </Typography>
-// </CardContent>
-// </Card>
